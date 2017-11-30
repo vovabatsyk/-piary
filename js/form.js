f14ed622ecf30ba05ca6c786ef8847a8a55966d8
vovabatsyk <vovabatsyk@gmail.com>
2017-11-30 10:42:45 +0200

    some changes

--- js/form.js
+++ js/form.js
@@ -1,29 +1,3 @@
-// Get the modal
-var modal = document.getElementById('formModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("inform");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeForm")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 $(function() {
     $('form input[type=submit]').click(function(e) {
         const form = $('form')[0];
@@ -42,9 +16,9 @@ $(function() {
             dataType: "json"
         }).done(function() {
             form.reset();
-            $('#message-box').html('<h2>Thank you!<h2>');
+            $('#message-box').html('<h2>Дякуєм!<h2>');
         }).fail(function() {
-            $('#message-box').html('Sorry, error occurred!');
+            $('#message-box').html('Ого, якась помилка');
         });
     });
     
--- js/modal-window.js
+++ js/modal-window.js
@@ -1,57 +1,56 @@
-function aboutModal() {	
-	// Get the modal
-	var modal = document.getElementById("aboutModal");
+function openAboutModal() {
+    // Get the modal
+    var modal = document.getElementById("aboutModal");
 
-	// Get the button that opens the modal
-	var btn = document.getElementById("about");
+    // Get the button that opens the modal
+    var btn = document.getElementById("about");
 
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	    modal.style.display = "block";
-	}
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
-	}
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 }
-about.addEventListener("click", aboutModal());
+about.addEventListener("click", openAboutModal());
 
-function healthModal() {	
-	// Get the modal
-	var modal = document.getElementById("healthModal");
+function openHealthModal() {
+    var modal = document.getElementById("healthModal");
+    var btn = document.getElementById("health");
+    var span = document.getElementsByClassName("close")[1];
 
-	// Get the button that opens the modal
-	var btn = document.getElementById("health");
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[1];
-
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	    modal.style.display = "block";
-	}
-
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
-
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
-	}
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 }
-health.addEventListener("click", healthModal());
+health.addEventListener("click", openHealthModal());
+
+function openFormModal() {
+    var modal = document.getElementById('formModal');
+    var btn = document.getElementById("inform");
+    var span = document.getElementsByClassName("closeForm")[0];
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+health.addEventListener("click", openFormModal());
